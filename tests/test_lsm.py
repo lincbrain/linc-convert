32f923637c4734d922c6e5fea1b8cfef83712121
Kaidong Chai <dev@kchai.me>
2024-11-07 13:35:32 -0500

    Add regression test for df modality, refactor, cleanup

--- tests/test_lsm.py
+++ tests/test_lsm.py
@@ -1,56 +1,15 @@
-import os
+""" """
+
 from pathlib import Path
-from tempfile import TemporaryDirectory
-from unittest import TestCase
 
 import numpy as np
 import tifffile
-import zarr
+from helper import _cmp_zarr_archives
 
 from linc_convert.modalities.lsm import mosaic
 
 
-def _compare_zarr_archives(path1, path2):
-    """
-    Compare two Zarr archives to check if they contain the same data.
-
-    Parameters
-    ----------
-    - path1 (str): Path to the first Zarr archive.
-    - path2 (str): Path to the second Zarr archive.
-
-    Returns
-    -------
-    - bool: True if both archives contain the same data, False otherwise.
-    """
-    # Open both Zarr groups
-    zarr1 = zarr.open(path1, mode="r")
-    zarr2 = zarr.open(path2, mode="r")
-
-    # Compare keys (dataset structure)
-    if zarr1.keys() != zarr2.keys():
-        return False
-    if zarr1.attrs != zarr2.attrs:
-        return False
-
-    # Compare each array in both archives
-    for key in zarr1.keys():
-        array1 = zarr1[key][:]
-        array2 = zarr2[key][:]
-
-        # Check for equality of the arrays
-        if not np.array_equal(array1, array2):
-            print(f"Mismatch found in dataset: {key}")
-            return False
-        if zarr1[key].attrs != zarr2[key].attrs:
-            return False
-
-    # If all checks pass
-    print("The Zarr archives are identical.")
-    return True
-
-
-def _write_test_data(directory):
+def _write_test_data(directory: str) -> None:
     root_path = Path(directory)
     for z in range(1, 2, 1):
         for y in range(1, 3, 1):
@@ -64,12 +23,8 @@ def _write_test_data(directory):
                     )
 
 
-class Test(TestCase):
-    def test_convert(self):
-        with TemporaryDirectory() as tmp_dir:
-            _write_test_data(tmp_dir)
-            output_zarr = os.path.join(tmp_dir, "output.zarr")
-            mosaic.convert(tmp_dir, output_zarr)
-            self.assertTrue(
-                _compare_zarr_archives(output_zarr, "tests/data/lsm.zarr.zip")
-            )
+def test_lsm(tmp_path):
+    _write_test_data(tmp_path)
+    output_zarr = tmp_path / "output.zarr"
+    mosaic.convert(str(tmp_path), str(output_zarr))
+    assert _cmp_zarr_archives(str(output_zarr), "data/lsm.zarr.zip")
