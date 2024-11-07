04dc22bf8f2f57f5279dd87bcb49ca0e57e78b70
Kaidong Chai <dev@kchai.me>
2024-11-07 13:35:32 -0500

    Add regression test for df modality, refactor, cleanup

---
+++ tests/test_df.py
@@ -0,0 +1,24 @@
+""" """
+
+import glob
+import os
+
+import glymur
+import numpy as np
+from helper import _cmp_zarr_archives
+
+from linc_convert.modalities.df import multi_slice
+
+
+def _write_test_data(directory: str) -> None:
+    for level in range(5):
+        image = np.zeros((1024, 768, 3), dtype=np.uint8) + level * 10
+        jp2 = glymur.Jp2k(f"{directory}/{level}.jp2", data=image)
+
+
+def test_df(tmp_path):
+    _write_test_data(tmp_path)
+    output_zarr = tmp_path / "output.zarr"
+    files = glob.glob(os.path.join(tmp_path, "*.jp2"))
+    multi_slice.convert(files, str(output_zarr))
+    assert _cmp_zarr_archives(str(output_zarr), "data/df.zarr.zip")
