5a72e9e1eb7bb43843c7e7687519c7bdbe207b25
Kaidong Chai <dev@kchai.me>
2024-11-14 15:05:57 -0500

    refactor: nifti-zarr header

--- linc_convert/modalities/psoct/stacking.py
+++ linc_convert/modalities/psoct/stacking.py
@@ -236,24 +236,157 @@ def convert(
     affine = orientation_to_affine(orientation, *vx[::-1])
     if center:
         affine = center_affine(affine, shape[:3])
-    header = nib.Nifti2Header()
-    header.set_data_shape(shape)
-    header.set_data_dtype(omz["0"].dtype)
-    header.set_qform(affine)
-    header.set_sform(affine)
-    header.set_xyzt_units(nib.nifti1.unit_codes.code[to_nifti_unit(unit)])
-    header.structarr["magic"] = b"nz2\0"
-    header = np.frombuffer(header.structarr.tobytes(), dtype="u1")
-    opt = {
-        "chunks": [len(header)],
-        "dimension_separator": r"/",
-        "order": "F",
-        "dtype": "|u1",
-        "fill_value": None,
-        "compressor": None,
+    niftizarr_write_header(omz,shape,affine,omz["0"].dtype,unit,nifti_version=2)
+    # header = nib.Nifti2Header()
+    # header.set_data_shape(shape)
+    # header.set_data_dtype(omz["0"].dtype)
+    # header.set_qform(affine)
+    # header.set_sform(affine)
+    # header.set_xyzt_units(nib.nifti1.unit_codes.code[to_nifti_unit(unit)])
+    # header.structarr["magic"] = b"nz2\0"
+    # header = np.frombuffer(header.structarr.tobytes(), dtype="u1")
+    # opt = {
+    #     "chunks": [len(header)],
+    #     "dimension_separator": r"/",
+    #     "order": "F",
+    #     "dtype": "|u1",
+    #     "fill_value": None,
+    #     "compressor": None,
+    # }
+    # omz.create_dataset("nifti", data=header, shape=len(header), **opt)
+    # print("done.")
+
+
+@contextmanager
+def mapmat(fnames, key=None):
+    """Load or memory-map an array stored in a .mat file"""
+    loaded_data = []
+
+    for fname in fnames:
+        try:
+            # "New" .mat file
+            f = h5py.File(fname, "r")
+        except Exception:
+            # "Old" .mat file
+            f = loadmat(fname)
+
+        if key is None:
+            if not len(f.keys()):
+                raise Exception(f"{fname} is empty")
+            key = list(f.keys())[0]
+            if len(f.keys()) > 1:
+                warn(
+                    f'More than one key in .mat file {fname}, arbitrarily loading "{key}"'
+                )
+
+        if key not in f.keys():
+            raise Exception(f"Key {key} not found in file {fname}")
+
+        if len(fnames) == 1:
+            yield f.get(key)
+            if hasattr(f, "close"):
+                f.close()
+            break
+        loaded_data.append(f.get(key))
+
+    yield np.stack(loaded_data, axis=-1)
+
+
+def make_json(oct_meta):
+    """
+    Expected input:
+    ---------------
+    Image medium: 60% TDE
+    Center Wavelength: 1294.84nm
+    Axial resolution: 4.9um
+    Lateral resolution: 4.92um
+    FOV: 3x3mm
+    Voxel size: 3x3x3um
+    Depth focus range: 225um
+    Number of focuses: 2
+    Focus #: 2
+    Slice thickness: 450um.
+    Number of slices: 75
+    Slice #:23
+    Modality: dBI
+    """
+
+    def parse_value_unit(string, n=None):
+        number = r"-?(\d+\.?\d*|\d*\.?\d+)(E-?\d+)?"
+        value = "x".join([number] * (n or 1))
+        match = re.fullmatch(r"(?P<value>" + value + r")(?P<unit>\w*)", string)
+        value, unit = match.group("value"), match.group("unit")
+        value = list(map(float, value.split("x")))
+        if n is None:
+            value = value[0]
+        return value, unit
+
+    meta = {
+        "BodyPart": "BRAIN",
+        "Environment": "exvivo",
+        "SampleStaining": "none",
     }
-    omz.create_dataset("nifti", data=header, shape=len(header), **opt)
-    print("done.")
+
+    for line in oct_meta.split("\n"):
+        if ":" not in line:
+            continue
+
+        key, value = line.split(":")
+        key, value = key.strip(), value.strip()
+
+        if key == "Image medium":
+            parts = value.split()
+            if "TDE" in parts:
+                parts[parts.index("TDE")] = "2,2' Thiodiethanol (TDE)"
+            meta["SampleMedium"] = " ".join(parts)
+
+        elif key == "Center Wavelength":
+            value, unit = parse_value_unit(value)
+            meta["Wavelength"] = value
+            meta["WavelengthUnit"] = unit
+
+        elif key == "Axial resolution":
+            value, unit = parse_value_unit(value)
+            meta["ResolutionAxial"] = value
+            meta["ResolutionAxialUnit"] = unit
+
+        elif key == "Lateral resolution":
+            value, unit = parse_value_unit(value)
+            meta["ResolutionLateral"] = value
+            meta["ResolutionLateralUnit"] = unit
+
+        elif key == "Voxel size":
+            value, unit = parse_value_unit(value, n=3)
+            meta["PixelSize"] = value
+            meta["PixelSizeUnits"] = unit
+
+        elif key == "Depth focus range":
+            value, unit = parse_value_unit(value)
+            meta["DepthFocusRange"] = value
+            meta["DepthFocusRangeUnit"] = unit
+
+        elif key == "Number of focuses":
+            value, unit = parse_value_unit(value)
+            meta["FocusCount"] = int(value)
+
+        elif key == "Slice thickness":
+            value, unit = parse_value_unit(value)
+            unit = convert_unit(value, unit[:-1], "u")
+            meta["SliceThickness"] = value
+
+        elif key == "Number of slices":
+            value, unit = parse_value_unit(value)
+            meta["SliceCount"] = int(value)
+
+        elif key == "Modality":
+            meta["OCTModality"] = value
+
+        else:
+            continue
+
+    return meta
+
+
 
 
 def generate_pyramid(
@@ -548,131 +681,264 @@ def generate_pyramid(
 #         shape_level = [x if i == no_pool else x // 2 for i, x in enumerate(shape_level)]
 #
 
-@contextmanager
-def mapmat(fnames, key=None):
-    """Load or memory-map an array stored in a .mat file"""
-    loaded_data = []
 
-    for fname in fnames:
-        try:
-            # "New" .mat file
-            f = h5py.File(fname, "r")
-        except Exception:
-            # "Old" .mat file
-            f = loadmat(fname)
 
-        if key is None:
-            if not len(f.keys()):
-                raise Exception(f"{fname} is empty")
-            key = list(f.keys())[0]
-            if len(f.keys()) > 1:
-                warn(
-                    f'More than one key in .mat file {fname}, arbitrarily loading "{key}"'
-                )
+def write_ome_metadata(
+    path: str | os.PathLike,
+    axes: list[str],
+    space_scale: float | list[float] = 1,
+    time_scale: float = 1,
+    space_unit: str = "micrometer",
+    time_unit: str = "second",
+    name: str = "",
+    pyramid_aligns: str | int | list[str | int] = 2,
+    levels: int | None = None,
+    zarr_version: int | None = None,
+) -> None:
+    """
+    Write OME metadata into Zarr.
 
-        if key not in f.keys():
-            raise Exception(f"Key {key} not found in file {fname}")
+    Parameters
+    ----------
+    path : str | PathLike
+        Path to parent Zarr.
+    axes : list[str]
+        Name of each dimension, in Zarr order (t, c, z, y, x)
+    space_scale : float | list[float]
+        Finest-level voxel size, in Zarr order (z, y, x)
+    time_scale : float
+        Time scale
+    space_unit : str
+        Unit of spatial scale (assumed identical across dimensions)
+    space_time : str
+        Unit of time scale
+    name : str
+        Name attribute
+    pyramid_aligns : float | list[float] | {"center", "edge"}
+        Whether the pyramid construction aligns the edges or the centers
+        of the corner voxels. If a (list of) number, assume that a moving
+        window of that size was used.
+    levels : int
+        Number of existing levels. Default: find out automatically.
+    zarr_version : {2, 3} | None
+        Zarr version. If `None`, guess from existing zarr array.
 
-        if len(fnames) == 1:
-            yield f.get(key)
-            if hasattr(f, "close"):
-                f.close()
+    """
+    path = UPath(path)
+
+    # Detect zarr version
+    if not zarr_version:
+        if (path / "0" / "zarr.json").exists():
+            zarr_version = 3
+        elif (path / "0" / ".zarray").exists():
+            zarr_version = 2
+        else:
+            raise FileNotFoundError("No existing array to guess version from")
+
+    # Read shape at each pyramid level
+    zname = "zarr.json" if zarr_version == 3 else ".zarray"
+    shapes = []
+    level = 0
+    while True:
+        if levels is not None and level > levels:
             break
-        loaded_data.append(f.get(key))
 
-    yield np.stack(loaded_data, axis=-1)
+        zpath = path / str(level) / zname
+        if not zpath.exists():
+            levels = level
+            break
 
+        level += 1
+        with zpath.open("rb") as f:
+            zarray = json.load(f)
+            shapes += [zarray["shape"]]
 
-def make_json(oct_meta):
-    """
-    Expected input:
-    ---------------
-    Image medium: 60% TDE
-    Center Wavelength: 1294.84nm
-    Axial resolution: 4.9um
-    Lateral resolution: 4.92um
-    FOV: 3x3mm
-    Voxel size: 3x3x3um
-    Depth focus range: 225um
-    Number of focuses: 2
-    Focus #: 2
-    Slice thickness: 450um.
-    Number of slices: 75
-    Slice #:23
-    Modality: dBI
-    """
-
-    def parse_value_unit(string, n=None):
-        number = r"-?(\d+\.?\d*|\d*\.?\d+)(E-?\d+)?"
-        value = "x".join([number] * (n or 1))
-        match = re.fullmatch(r"(?P<value>" + value + r")(?P<unit>\w*)", string)
-        value, unit = match.group("value"), match.group("unit")
-        value = list(map(float, value.split("x")))
-        if n is None:
-            value = value[0]
-        return value, unit
-
-    meta = {
-        "BodyPart": "BRAIN",
-        "Environment": "exvivo",
-        "SampleStaining": "none",
+    axis_to_type = {
+        "x": "space",
+        "y": "space",
+        "z": "space",
+        "t": "time",
+        "c": "channel",
     }
 
-    for line in oct_meta.split("\n"):
-        if ":" not in line:
-            continue
+    # Number of spatial (s), batch (b) and total (n) dimensions
+    ndim = len(axes)
+    sdim = sum(axis_to_type[axis] == "space" for axis in axes)
+    bdim = ndim - sdim
 
-        key, value = line.split(":")
-        key, value = key.strip(), value.strip()
+    if isinstance(pyramid_aligns, (int, str)):
+        pyramid_aligns = [pyramid_aligns]
+    pyramid_aligns = list(pyramid_aligns)
+    if len(pyramid_aligns) < sdim:
+        repeat = pyramid_aligns[:1] * (sdim - len(pyramid_aligns))
+        pyramid_aligns = repeat + pyramid_aligns
+    pyramid_aligns = pyramid_aligns[-sdim:]
 
-        if key == "Image medium":
-            parts = value.split()
-            if "TDE" in parts:
-                parts[parts.index("TDE")] = "2,2' Thiodiethanol (TDE)"
-            meta["SampleMedium"] = " ".join(parts)
+    if isinstance(space_scale, (int, float)):
+        space_scale = [space_scale]
+    space_scale = list(space_scale)
+    if len(space_scale) < sdim:
+        repeat = space_scale[:1] * (sdim - len(space_scale))
+        space_scale = repeat + space_scale
+    space_scale = space_scale[-sdim:]
 
-        elif key == "Center Wavelength":
-            value, unit = parse_value_unit(value)
-            meta["Wavelength"] = value
-            meta["WavelengthUnit"] = unit
+    multiscales = [
+        {
+            "version": "0.4",
+            "axes": [
+                {
+                    "name": axis,
+                    "type": axis_to_type[axis],
+                }
+                if axis_to_type[axis] == "channel"
+                else {
+                    "name": axis,
+                    "type": axis_to_type[axis],
+                    "unit": (
+                        space_unit
+                        if axis_to_type[axis] == "space"
+                        else time_unit
+                        if axis_to_type[axis] == "time"
+                        else None
+                    ),
+                }
+                for axis in axes
+            ],
+            "datasets": [],
+            "type": "median window " + "x".join(["2"] * sdim),
+            "name": name,
+        }
+    ]
 
-        elif key == "Axial resolution":
-            value, unit = parse_value_unit(value)
-            meta["ResolutionAxial"] = value
-            meta["ResolutionAxialUnit"] = unit
+    shape = shape0 = shapes[0]
+    for n in range(len(shapes)):
+        shape = shapes[n]
+        multiscales[0]["datasets"].append({})
+        level = multiscales[0]["datasets"][-1]
+        level["path"] = str(n)
 
-        elif key == "Lateral resolution":
-            value, unit = parse_value_unit(value)
-            meta["ResolutionLateral"] = value
-            meta["ResolutionLateralUnit"] = unit
+        scale = [1] * bdim + [
+            (
+                pyramid_aligns[i] ** n
+                if not isinstance(pyramid_aligns[i], str)
+                else (shape0[bdim + i] / shape[bdim + i])
+                if pyramid_aligns[i][0].lower() == "e"
+                else ((shape0[bdim + i] - 1) / (shape[bdim + i] - 1))
+            )
+            * space_scale[i]
+            for i in range(sdim)
+        ]
+        translation = [0] * bdim + [
+            (
+                pyramid_aligns[i] ** n - 1
+                if not isinstance(pyramid_aligns[i], str)
+                else (shape0[bdim + i] / shape[bdim + i]) - 1
+                if pyramid_aligns[i][0].lower() == "e"
+                else 0
+            )
+            * 0.5
+            * space_scale[i]
+            for i in range(sdim)
+        ]
 
-        elif key == "Voxel size":
-            value, unit = parse_value_unit(value, n=3)
-            meta["PixelSize"] = value
-            meta["PixelSizeUnits"] = unit
+        level["coordinateTransformations"] = [
+            {
+                "type": "scale",
+                "scale": scale,
+            },
+            {
+                "type": "translation",
+                "translation": translation,
+            },
+        ]
 
-        elif key == "Depth focus range":
-            value, unit = parse_value_unit(value)
-            meta["DepthFocusRange"] = value
-            meta["DepthFocusRangeUnit"] = unit
+    scale = [1] * ndim
+    if "t" in axes:
+        scale[axes.index("t")] = time_scale
+    multiscales[0]["coordinateTransformations"] = [{"scale": scale, "type": "scale"}]
 
-        elif key == "Number of focuses":
-            value, unit = parse_value_unit(value)
-            meta["FocusCount"] = int(value)
+    if zarr_version == 3:
+        with (path / "zarr.json").open("wt") as f:
+            json.dump(
+                {
+                    "zarr_format": 3,
+                    "node_type": "group",
+                    "attributes": {
+                        # Zarr v2 way of doing it -- neuroglancer wants this
+                        "multiscales": multiscales,
+                        # Zarr RFC-2 recommended way
+                        "ome": {"version": "0.4", "multiscales": multiscales},
+                    },
+                },
+                f,
+                indent=4,
+            )
+    else:
+        multiscales[0]["version"] = "0.4"
+        with (path / ".zgroup").open("wt") as f:
+            json.dump({"zarr_format": 2}, f, indent=4)
+        with (path / ".zattrs").open("wt") as f:
+            json.dump({"multiscales": multiscales}, f, indent=4)
 
-        elif key == "Slice thickness":
-            value, unit = parse_value_unit(value)
-            unit = convert_unit(value, unit[:-1], "u")
-            meta["SliceThickness"] = value
 
-        elif key == "Number of slices":
-            value, unit = parse_value_unit(value)
-            meta["SliceCount"] = int(value)
 
-        elif key == "Modality":
-            meta["OCTModality"] = value
+def niftizarr_write_header(
+    omz,
+    shape: list[int],
+    affine: np.ndarray,
+    dtype: np.dtype | str,
+    unit: Literal["micron", "mm"] | None = None,
+    header: nib.Nifti1Header | nib.Nifti2Header | None = None,
+    nifti_version: Literal[1,2] = 1
+) -> None:
+    """
+    Write NIfTI header in a NIfTI-Zarr file.
 
-        else:
-            continue
+    Parameters
+    ----------
+    path : PathLike | str
+        Path to parent Zarr.
+    affine : (4, 4) matrix
+        Orientation matrix.
+    shape : list[int]
+        Array shape, in NIfTI order (x, y, z, t, c).
+    dtype : np.dtype | str
+        Data type.
+    unit : {"micron", "mm"}, optional
+        World unit.
+    header : nib.Nifti1Header | nib.Nifti2Header, optional
+        Pre-instantiated header.
+    zarr_version : int, default=3
+        Zarr version.
+    """
+    # TODO: we do not write the json zattrs, but it should be added in
+    #       once the nifti-zarr package is released
 
-    return meta
+    # If dimensions do not fit in a short (which is often the case), we
+    # use NIfTI 2.
+    if all(x < 32768 for x in shape) or nifti_version == 1:
+        NiftiHeader = nib.Nifti1Header
+    else:
+        NiftiHeader = nib.Nifti2Header
+
+    header = header or NiftiHeader()
+    header.set_data_shape(shape)
+    header.set_data_dtype(dtype)
+    header.set_qform(affine)
+    header.set_sform(affine)
+    if unit:
+        header.set_xyzt_units(nib.nifti1.unit_codes.code[unit])
+    header = np.frombuffer(header.structarr.tobytes(), dtype="u1")
+
+
+    metadata = {
+        "chunks": [len(header)],
+        "order": "F",
+        "dtype": "|u1",
+        "fill_value": None,
+        "compressor": None, # TODO: Subject to change
+    }
+
+    omz.create_dataset("nifti", data=header, shape=len(header), **metadata)
+
+    print("done.")
