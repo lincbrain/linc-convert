ad90a048f1b1040b6a7e81c6bd819330a9f52cac
balbasty <balbasty@users.noreply.github.com>
2024-11-21 19:19:51 +0000

    style fixes by ruff

--- linc_convert/modalities/lsm/__init__.py
+++ linc_convert/modalities/lsm/__init__.py
@@ -2,6 +2,7 @@
 
 try:
     import tifffile as _  # noqa: F401
+
     __all__ = ["cli", "mosaic"]
     from . import cli, mosaic
 except ImportError:
--- linc_convert/modalities/psoct/__init__.py
+++ linc_convert/modalities/psoct/__init__.py
@@ -3,6 +3,7 @@
 try:
     import h5py as _h5py  # noqa: F401
     import scipy as _scipy  # noqa: F401
+
     __all__ = ["cli", "multi_slice", "single_volume"]
     from . import cli, multi_slice, single_volume
 except ImportError:
