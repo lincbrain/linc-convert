f65560d6410d74767b6d7ed0534ddbd31b494a39
Kaidong Chai <dev@kchai.me>
2025-03-28 13:16:11 -0400

    feat: add lsm spool

--- linc_convert/modalities/lsm/__init__.py
+++ linc_convert/modalities/lsm/__init__.py
@@ -3,7 +3,7 @@
 try:
     import tifffile as _  # noqa: F401
 
-    __all__ = ["cli", "mosaic", "multi_slice"]
-    from . import cli, mosaic, multi_slice
+    __all__ = ["cli", "mosaic", "multi_slice", "spool"]
+    from . import cli, mosaic, multi_slice, spool
 except ImportError:
     pass
