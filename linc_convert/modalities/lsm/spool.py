8a6c282b7f5a19f1f49e82e5a6ba4854504c690b
Kaidong Chai <dev@kchai.me>
2025-03-31 13:47:34 -0400

    feat: use new spool reader

--- linc_convert/modalities/lsm/spool.py
+++ linc_convert/modalities/lsm/spool.py
@@ -114,30 +114,29 @@ def convert(
         raise ValueError("No tile folder found")
     
     all_tiles_info = []
+    tiles_info_by_index = {}
     TileInfo = namedtuple(
         "TileInfo",
-        ["prefix", "run", "y", "z", "suffix", "filename", "array"]
+        ["prefix", "run", "y", "z", "suffix", "filename", "reader"]
     )
     for tile_folder_name in all_tiles_folders_names:
         if tile_folder_name.endswith(r"/"):
             tile_folder_name = tile_folder_name[:-1]
         parsed = CHUNK_PATTERN.fullmatch(os.path.basename(tile_folder_name))
-        all_tiles_info.append(
-            TileInfo(
+        tile =TileInfo(
                 parsed.group("prefix"),
                 int(parsed.group("run")),
                 int(parsed.group("y")),
                 int(parsed.group("z")),
                 parsed.group("suffix"),
                 tile_folder_name,
-                read_spool_files(
-                    inp,
-                    tile_folder_name,
-                    os.path.join(inp, f"{tile_folder_name}_info.mat"),
-                    no_load=True
-                )
+                SpoolSetInterpreter(tile_folder_name)
             )
-        )
+        all_tiles_info.append(tile)
+        # check no duplication
+        if (tile.y, tile.z) in tiles_info_by_index:
+            raise ValueError("Duplicate tile")
+        tiles_info_by_index[(tile.y, tile.z)] = tile
 
     # default output name
     if not out:
@@ -149,13 +148,7 @@ def convert(
     z_tiles = set(tile.z for tile in all_tiles_info)
     y_tiles = set(tile.y for tile in all_tiles_info)
     num_y_tiles, num_z_tiles = len(y_tiles), len(z_tiles)
-    tiles_info_by_index = {}
-    # check no duplicate
-    for tile in all_tiles_info:
-        if (tile.y, tile.z) in tiles_info_by_index:
-            raise ValueError("Duplicate tile")
-        tiles_info_by_index[(tile.y, tile.z)] = tile
-        
+
     # tile array is y,z,x
     shapes_x = defaultdict(list)
     dtypes = defaultdict(list)
@@ -168,17 +161,19 @@ def convert(
             if tile is None:
                 warnings.warn(f"Missing tile {y_tile}, {z_tile}")
                 continue
-            arr = tile.array
-            sx, sy, sz = arr.shape[0], arr.shape[1], arr.shape[2]
-            dt = arr.dtype
+            reader = tile.reader
+            sx, sz, sy = reader.spool_shape
+            sx *= len(list(reader._get_spool_names_in_order()))
+            print(sx, sy, sz)
+            dt = reader.dtype
             # Collect X shapes and dtypes.
-            shapes_x[sz].append((y_tile, z_tile))
+            shapes_x[sx].append((y_tile, z_tile))
             dtypes[dt].append((y_tile, z_tile))
 
             # Collect Y shapes per z_tile.
-            shapes_y_by_z[z_tile][sx].append((y_tile, z_tile))
+            shapes_y_by_z[z_tile][sy].append((y_tile, z_tile))
             # Collect Z shapes per y_tile.
-            shapes_z_by_y[y_tile][sy].append((y_tile, z_tile))
+            shapes_z_by_y[y_tile][sz].append((y_tile, z_tile))
 
     # Check consistency for X shapes and dtypes.
     if len(shapes_x) != 1:
@@ -202,9 +197,9 @@ def convert(
     # Calculate full shapes using the assumption that the first tile in each direction represents the size.
     # Note: This assumes that a tile at index (y_tile, 1) or (1, z_tile) exists.
     full_shape_x = next(iter(shapes_x))
-    full_shape_y = sum(tiles_info_by_index[(y_tile, 1)].array.shape[0] for y_tile in
+    full_shape_y = sum(tiles_info_by_index[(y_tile, 1)].reader.spool_shape[2] for y_tile in
                        range(1, num_y_tiles + 1)) - (num_y_tiles-1) * overlap
-    full_shape_z = sum(tiles_info_by_index[(1, z_tile)].array.shape[1] for z_tile in
+    full_shape_z = sum(tiles_info_by_index[(1, z_tile)].reader.spool_shape[1] for z_tile in
                        range(1, num_z_tiles + 1))
     fullshape = [full_shape_z, full_shape_y, full_shape_x]
 
@@ -232,23 +227,17 @@ def convert(
     for i, tile_info in enumerate(all_tiles_info):
         tile_z  = tile_info.z - 1
         tile_y = tile_info.y - 1
-        dat = read_spool_files(
-            inp,
-            tile_info.filename,
-            os.path.join(inp, f"{tile_folder_name}_info.mat")
-        ).swapaxes(0, 1)
-
+        dat = tile_info.reader.assemble()
         
         if num_y_tiles != 1 and overlap != 0:
             if tile_info.y != min(y_tiles):
                 dat = dat[:, overlap // 2:, :]
             if tile_info.y != max(y_tiles):
-                dat = dat[:, :-overlap // 2 - (overlap - 2 * (overlap // 2)), :]
+                dat = dat[:, :-overlap // 2 - (overlap % 2), :]
 
-        zstart = sum(tiles_info_by_index[(1, z+1)].array.shape[1] for z in range(tile_z))
-        ystart = sum(tiles_info_by_index[(y+1, tile_info.z)].array.shape[0] - overlap for y in range(tile_y))
-        # zstart = sum(shape[0][0] for shape in allshapes[:tile_z])
-        # ystart = sum(shape[1] - overlap for shape in allshapes[tile_z][:tile_y])
+        zstart = sum(tiles_info_by_index[(1, z+1)].reader.spool_shape[1] for z in range(tile_z))
+        ystart = sum(tiles_info_by_index[(y+1, tile_info.z)].spool_shape[2] - overlap for y in range(tile_y))
+
         if tile_y != 0:
             ystart += overlap // 2
         print(
@@ -558,4 +547,168 @@ def read_spool_files(scan_path: str, scan_name: str, mat_path: str, no_load: boo
 
     return scape_data
 
+
+# from compression_tools.alt_zip import alt_zip
+import configparser
+import numpy as np
+import io
+import warnings
+import os
+
+
+
+class SpoolSetInterpreter:
+
+    def __init__(self,compression_tools_zip_file):
+        if os.path.isfile(compression_tools_zip_file) and os.path.splitext(compression_tools_zip_file)[-1] == '.zip':
+            self.type = '.zip'
+            self.compression_tools_zip_file = compression_tools_zip_file
+            self.location = compression_tools_zip_file
+            self.spool_set = alt_zip(self.location)
+        elif os.path.isdir(compression_tools_zip_file):
+            self.type = 'dir'
+            self.parent = compression_tools_zip_file
+            self.file_list = glob(os.path.join(compression_tools_zip_file,'*'))
+            self.spool_set = tuple([os.path.split(x)[-1] for x in self.file_list])
+        else:
+            assert False, 'The input data structure is not a ZIP file or Directory'
+
+
+        self._what_spool_format()
+        self.spool_files = tuple(self._get_spool_names_in_order()) # In order
+        self._get_acquisitionparameters_str()
+        self._get_config()
+        self._extract_config_values()
+
+    def _make_filename_from_spool_set(self,spool_entry):
+        return os.path.join(self.parent, spool_entry)
+
+    @property
+    def entries(self):
+        if self.type == '.zip':
+            return self.spool_set.entries
+        elif self.type == 'dir':
+            return self.spool_set
+
+
+    def _what_spool_format(self):
+        if 'Spooled files.sifx' in self.entries:
+            format = 'zyla'
+        else:
+            raise TypeError("Unknown or unsupported spool file format")
+
+        self.format = format
+
+    def _list_spool_files(self):
+        return sorted(
+            tuple(
+            [x for x in self.entries if '0spool.dat' in x]
+        )
+        )
+    def _get_acquisitionparameters_str(self):
+        if self.type == '.zip':
+            ini = self.spool_set['acquisitionmetadata.ini']
+        elif self.type == 'dir':
+            file = self._make_filename_from_spool_set('acquisitionmetadata.ini')
+            with open(file,'rb') as f:
+                ini = f.read()
+        ini = ini.decode('UTF-8-sig')  # Encoding for acquisitionmetadata.ini
+        self.acquisitionparameters_str = ini
+
+    def _get_config(self):
+        buf = io.StringIO(self.acquisitionparameters_str)
+        self.config = configparser.ConfigParser()
+        self.config.read_file(buf)
+
+    def _extract_config_values(self):
+        self.acquisition_metadata = {}
+        # ini info
+        self.acquisition_metadata['height'] = self.config.getint('data', 'AOIHeight')
+        self.acquisition_metadata['width'] = self.config.getint('data', 'AOIWidth')
+        self.acquisition_metadata['stride'] = self.config.getint('data', 'AOIStride')
+        dtype = self.config.get('data', 'PixelEncoding')
+
+        if dtype == 'Mono16':
+            dtype = np.dtype('uint16')
+        elif dtype == 'Mono8':
+            dtype = np.dtype('uint8')
+
+        self.acquisition_metadata['dtype'] = dtype
+        self.dtype = dtype
+
+        self.spool_nbytes = self.config.getint('data', 'ImageSizeBytes')
+        self.acquisition_metadata['nbytes'] = self.spool_nbytes
+
+        self.acquisition_metadata['images'] = self.config.getint('multiimage', 'ImagesPerFile')
+
+        numDepths = self.acquisition_metadata['height']
+        numLatPix = self.acquisition_metadata['stride'] // 2
+        imageBytes = self.acquisition_metadata['nbytes']
+        numFramesPerSpool = self.acquisition_metadata['images']
+        startIndex = self.acquisition_metadata['nbytes']
+        imageSize = self.acquisition_metadata['nbytes']
+
+        numRows = numDepths + 2
+
+        if numDepths % 2:  # if there is an odd number of rows ->  KPEDIT - odd rows means 1 less column for some reason
+            numRows = numDepths + 1
+
+        numColumns = numLatPix
+
+        self.spool_shape = (numFramesPerSpool, numRows, numColumns)
+
+    def _load_spool_file(self,spool_file_name):
+        if self.type == '.zip':
+            array = np.frombuffer(self.spool_set[spool_file_name],dtype=self.dtype)
+        elif self.type == 'dir':
+            file = self._make_filename_from_spool_set(spool_file_name)
+            print(f'Reading file {spool_file_name}')
+            with open(file, 'rb') as f:
+                array = np.frombuffer(f.read(),dtype=self.dtype)
+        return np.reshape(array, self.spool_shape)
+
+    def __getitem__(self,key):
+        if isinstance(key,str):
+            assert key in self.spool_files, 'Must be a spool file in self.spool_files OR integer index in self.spool_files'
+            return self._load_spool_file(key)
+        elif isinstance(key,int):
+            return self._load_spool_file(self.spool_files[key])
+
+    def __iter__(self):
+        yield from (self[x] for x in range(len(self)))
+
+    def __contains__(self, item):
+        return item in self.spool_files
+
+    def __len__(self):
+        return len(self.spool_files)
+
+
+    def _get_spool_names_in_order(self):
+        '''
+        Spool files are ordered sequentially in the order they were collected 0,1,2,...,201,202,203,...
+        but file names are recorded as the reversed number padded to
+        10 digits (0000000000,1000000000,20000000000,...,1020000000,2020000000,3020000000,...) + spool.dat
+        '''
+        spool_files = self._list_spool_files()
+        misses=0
+        for idx in range(len(spool_files)):
+            # Convert index to string, pad with zeros to 10 digits and reverse
+            tmp = str(idx+misses).zfill(10)[::-1]
+            tmp = f'{tmp}spool.dat'
+            if tmp in spool_files:
+                yield tmp
+            else:
+                warnings.warn(f"{tmp} not located in spool directory")
+                misses += 1
+
+    def assemble(self):
+        axis_0_shape = self.spool_shape[0]
+        canvas = np.zeros((axis_0_shape*len(self),*self.spool_shape[1:]), dtype=self.dtype)
+        for idx,spool_file in enumerate(self):
+            start = idx*axis_0_shape
+            stop = start + axis_0_shape
+            canvas[start:stop] = spool_file
+        print(canvas.shape)
+        return canvas.transpose(1,2,0)
 
