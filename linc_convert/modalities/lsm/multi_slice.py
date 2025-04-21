b07ed7ac9776846888e31f3c4caa08b70b92d302
Calvin Chai <kchai1@mgh.harvard.edu>
2025-04-21 14:30:35 -0400

    Update linc_convert/modalities/lsm/multi_slice.py
    Co-authored-by: Kabilar Gunalan <kabi@mit.edu>

--- linc_convert/modalities/lsm/multi_slice.py
+++ linc_convert/modalities/lsm/multi_slice.py
@@ -55,7 +55,7 @@ def convert(
     `{"L", "R", "A", "P", "I", "S"}`, where
 
     * the first letter corresponds to the horizontal dimension and
-        indicates the anatomical meaning of the _right_ of the jp2 image,
+        indicates the anatomical meaning of the _right_ of the image,
     * the second letter corresponds to the vertical dimension and
         indicates the anatomical meaning of the _bottom_ of the image.
 
