53fa498705843fe06343244e3ec8fcc0b765a7db
Calvin Chai <kchai1@mgh.harvard.edu>
2025-04-21 14:29:50 -0400

    Update linc_convert/modalities/lsm/multi_slice.py
    Co-authored-by: Kabilar Gunalan <kabi@mit.edu>

--- linc_convert/modalities/lsm/multi_slice.py
+++ linc_convert/modalities/lsm/multi_slice.py
@@ -71,7 +71,7 @@ def convert(
     ----------
     inp
         Path to the root directory, which contains a collection of
-        subfolders named `*_z{:02d}_y{:02d}*`, each containing a
+        subfolders named `*_y{:02d}_z{:02d}*`, each containing a
         collection of files named `*_plane{:03d}_c{:d}.tiff`.
     out
         Path to the output Zarr directory [<INP>.ome.zarr]
