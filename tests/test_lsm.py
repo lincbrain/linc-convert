8029ed3119e1f53d103dea3630ff1647eb4b2f03
Kabilar Gunalan <kabi@mit.edu>
2025-05-16 14:03:09 -0500

    Update subprocess call

--- tests/test_lsm.py
+++ tests/test_lsm.py
@@ -35,7 +35,7 @@ def test_lsm(tmp_path):
 def test_transfer():
         
     input_dir = './000051/sourcedata/sub-test1'
-    subprocess.run(f"linc-convert lsm transfer --input-dir '{input_dir}' --dandiset-url 'https://lincbrain.org/dandiset/000051' --dandi-instance 'linc' --subject 'test1' --output-dir '.' --max-size-gb 0.02 --no-upload", shell=True, check=True)
+    subprocess.Popen(f"linc-convert lsm transfer --input-dir '{input_dir}' --dandiset-url 'https://lincbrain.org/dandiset/000051' --dandi-instance 'linc' --subject 'test1' --output-dir '.' --max-size-gb 0.02 --no-upload", env=os.environ.copy(), shell=True, check=True)
 
     extract_dir = './sub-test1'
     os.mkdir(extract_dir)
