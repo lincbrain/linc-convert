1b6373e0ca6a1a7cdfeba783c812a26758e19f12
Kaidong Chai <dev@kchai.me>
2025-06-24 10:34:31 -0400

    chore: disable file logger for now,

--- linc_convert/utils/logging.py
+++ linc_convert/utils/logging.py
@@ -7,19 +7,16 @@ def setup_logging():
         level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
-
     logging.captureWarnings(True)
 
 
 def add_file_handler(log_file_path=None):
-    # Create a file handler to save logs
-    log_filename = "/scratch/converter.log"
-    file_handler = logging.FileHandler(log_filename)
+    file_handler = logging.FileHandler(log_file_path)
     file_handler.setLevel(logging.DEBUG)
     file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(file_formatter)
     logger.addHandler(file_handler)
 
-
+#TODO: add file handler for each platform. prob ref dandi-ci. prevent logging error in unit test
 setup_logging()
-add_file_handler()
+# add_file_handler("/scratch/converter.log")
