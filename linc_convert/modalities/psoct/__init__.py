e3bbfa6eb8c2ebbb3a831e21dae907623763cadb
Kaidong Chai <dev@kchai.me>
2025-06-26 14:11:05 -0400

    feat: oct pipelines

--- linc_convert/modalities/psoct/__init__.py
+++ linc_convert/modalities/psoct/__init__.py
@@ -4,8 +4,8 @@ try:
     import h5py as _h5py  # noqa: F401
     import scipy as _scipy  # noqa: F401
 
-    __all__ = ["cli", "multi_slice", "single_volume"]
+    __all__ = ["cli", "multi_slice", "single_volume", "mosaic3d", "mosaic2d", "pyramid"]
 
-    from . import cli, multi_slice, single_volume
+    from . import cli, multi_slice, single_volume, mosaic3d, mosaic2d, pyramid
 except ImportError:
     pass
