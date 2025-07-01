809b8b2cfcba79ed4be1fc06d7c2c8b4996b1652
Kaidong Chai <dev@kchai.me>
2025-07-01 13:32:41 -0400

    fix: add dependencies

--- linc_convert/utils/zarr/zarr_io/__init__.py
+++ linc_convert/utils/zarr/zarr_io/__init__.py
@@ -7,8 +7,9 @@ from .abc import ZarrNode, ZarrArray, ZarrGroup
 from .drivers.zarr_python import ZarrPythonArray, ZarrPythonGroup
 
 try:
-    from .drivers.tensorstore import ZarrTSArray, ZarrTSGroup
+    import tensorstore as TS
 except ImportError:
     warnings.warn("Tensorstore is not installed, driver disabled")
+from .drivers.tensorstore import ZarrTSArray, ZarrTSGroup
 
 from .factory import open, open_group, from_config
