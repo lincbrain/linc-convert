93cb341cb603de8b1028ee80a21afb8f639943df
Kaidong Chai <dev@kchai.me>
2025-07-08 13:04:10 -0400

    wip: z_stitching.py

--- linc_convert/modalities/psoct/__init__.py
+++ linc_convert/modalities/psoct/__init__.py
@@ -4,8 +4,8 @@ try:
     import h5py as _h5py  # noqa: F401
     import scipy as _scipy  # noqa: F401
 
-    __all__ = ["cli", "multi_slice", "single_volume", "mosaic3d", "mosaic2d", "pyramid"]
+    __all__ = ["cli", "multi_slice", "single_volume", "mosaic3d", "mosaic2d", "pyramid", "z_stitching"]
 
-    from . import cli, multi_slice, single_volume, mosaic3d, mosaic2d, pyramid
+    from . import cli, multi_slice, single_volume, mosaic3d, mosaic2d, pyramid, z_stitching
 except ImportError:
     pass
