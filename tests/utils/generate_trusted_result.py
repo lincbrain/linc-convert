92cfefd706b58c33a589deecaf2cac683fd0338a
Kaidong Chai <dev@kchai.me>
2025-07-10 10:35:52 -0400

    tests: refresh regression test for df pipelines

--- tests/utils/generate_trusted_result.py
+++ tests/utils/generate_trusted_result.py
@@ -14,16 +14,6 @@ from linc_convert.modalities.lsm import mosaic
 from linc_convert.modalities.wk import webknossos_annotation
 
 if __name__ == "__main__":
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        test_df._write_test_data(tmp_dir)
-        output_zarr = os.path.join(tmp_dir, "output.zarr")
-        files = glob.glob(os.path.join(tmp_dir, "*.jp2"))
-        files.sort()
-        with zipfile.ZipFile("data/df_input.zip", "w") as z:
-            for file in files:
-                z.write(file, os.path.basename(file))
-        multi_slice.convert(files, output_zarr)
-        zarr.copy_all(zarr.open(output_zarr), zarr.open("data/df.zarr.zip", "w"))
 
     with tempfile.TemporaryDirectory() as tmp_dir:
         test_lsm._write_test_data(tmp_dir)
