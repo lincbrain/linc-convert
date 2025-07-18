ef71641c389f13cfce0331326c945dec6622689d
Kaidong Chai <dev@kchai.me>
2025-07-18 11:14:19 -0400

    fix: update to new api

--- linc_convert/modalities/psoct/mosaic3d.py
+++ linc_convert/modalities/psoct/mosaic3d.py
@@ -1,5 +1,7 @@
 """
-This module includes components that are based on the https://github.com/CarolineMagnain/OCTAnalysis repository (currently private).  We have permission from the owner to include the code here.
+This module includes components that are based on the
+https://github.com/CarolineMagnain/OCTAnalysis repository (currently private).  We
+have permission from the owner to include the code here.
 """
 
 import logging
@@ -14,23 +16,21 @@ import dask.array as da
 import nibabel as nib
 import numpy as np
 import scipy.io as sio
-import tensorstore as ts
 import tqdm
-import zarr
 from cyclopts import Parameter
 from dask import delayed
 from dask.diagnostics import ProgressBar
 from niizarr import default_nifti_header, write_ome_metadata
 
+from linc_convert.modalities.psoct._utils import (atleast_2d_trailing,
+                                                  find_experiment_params,
+                                                  struct_arr_to_dict)
 from linc_convert.modalities.psoct.cli import psoct
-from linc_convert.modalities.psoct.mosaic2d import _normalize_tile_coords, \
-    _compute_blending_ramp, _load_nifti_tile, _load_tile
-from linc_convert.modalities.psoct._utils import struct_arr_to_dict, \
-    find_experiment_params, atleast_2d_trailing
-from linc_convert.utils.zarr.zarr_io.drivers.tensorstore import default_write_config
-from linc_convert.utils.zarr.zarr_io.drivers.zarr_python import compute_zarr_layout
-from linc_convert.utils.zarr.generate_pyramid import generate_pyramid
-from linc_convert.utils.zarr.zarr_config import ZarrConfig
+from linc_convert.modalities.psoct.mosaic2d import (_compute_blending_ramp,
+                                                    _load_nifti_tile, _load_tile,
+                                                    _normalize_tile_coords)
+from linc_convert.utils.io.zarr import from_config
+from linc_convert.utils.zarr_config import ZarrConfig
 
 logger = logging.getLogger(__name__)
 mosaic3d = cyclopts.App(name="mosaic3d", help_format="markdown")
@@ -38,6 +38,7 @@ psoct.command(mosaic3d)
 
 # 3d data has pixdim incorrectly set and cause nibabel keep logging warning
 nib.imageglobals.logger.setLevel(40)
+
 
 @mosaic3d.default
 def mosaic3d_telesto(
@@ -51,7 +52,7 @@ def mosaic3d_telesto(
         downsample: bool = False,
         use_gpu: bool = False,
         zarr_config: ZarrConfig = None,
-) -> None:
+        ) -> None:
     """
     Parameters
     ----------
@@ -83,19 +84,21 @@ def mosaic3d_telesto(
     tile_width, tile_height = tile_size - clip_x, tile_size - clip_y
     if tilted_illumination:
         tile_width = int(exp_params['NbPix' + tilt_postfix]) - clip_x
-    x_coords, y_coords = _normalize_tile_coords(exp_params['X_Mean' + tilt_postfix], exp_params['Y_Mean' + tilt_postfix],
-    )
+    x_coords, y_coords = _normalize_tile_coords(exp_params['X_Mean' + tilt_postfix],
+                                                exp_params['Y_Mean' + tilt_postfix],
+                                                )
     full_width = int(np.nanmax(x_coords) + tile_width)
     full_height = int(np.nanmax(y_coords) + tile_height)
     depth = int(mosaic_info['MZL'])
 
     blend_ramp = _compute_blending_ramp(tile_width, tile_height, x_coords, y_coords)
 
-    map_indices = exp_params['MapIndex_Tot_offset' + tilt_postfix] + exp_params['First_Tile' + tilt_postfix] - 1
+    map_indices = exp_params['MapIndex_Tot_offset' + tilt_postfix] + exp_params[
+        'First_Tile' + tilt_postfix] - 1
 
     no_tilted_illumination_scan = scan_info['TiltedIllumination'] != 'Yes'
 
-    focus = _load_nifti_tile(scan_info['FocusFile'+ tilt_postfix] )
+    focus = _load_nifti_tile(scan_info['FocusFile' + tilt_postfix])
     raw_tile_width = int(scan_info['NbPixels' + tilt_postfix])
 
     # slice_idx_in, slice_idx_out, slice_idx_run = slice_indices[:, slice_index]
@@ -123,25 +126,24 @@ def mosaic3d_telesto(
     else:
         temp_compensate = False
 
-    chunk, shard = compute_zarr_layout((depth, full_height, full_width), np.float32,
-                                       zarr_config)
+    chunk, shard = zarr_config.chunk, zarr_config.shard
 
     dBI_result, R3D_result, O3D_result = build_slice(mosaic_idx,
-                                                                          input_file_template,
+                                                     input_file_template,
                                                      blend_ramp, clip_x, clip_y,
                                                      full_width, full_height, depth,
                                                      flip_z, map_indices, scan_info,
                                                      tile_width, tile_height, x_coords,
-                                                                          y_coords,
-                                                                          raw_tile_width,
-                                                                          focus,
-                                                                          temp_compensate,
-                                                                          chunk=chunk if not shard else shard)
+                                                     y_coords,
+                                                     raw_tile_width,
+                                                     focus,
+                                                     temp_compensate,
+                                                     chunk=chunk if not shard else
+                                                     shard)
 
-
-    dBI_result = dBI_result.transpose(2,1,0)
-    R3D_result = R3D_result.transpose(2,1,0)
-    O3D_result = O3D_result.transpose(2,1,0)
+    dBI_result = dBI_result.transpose(2, 1, 0)
+    R3D_result = R3D_result.transpose(2, 1, 0)
+    O3D_result = O3D_result.transpose(2, 1, 0)
 
     writers = []
     results = []
@@ -153,14 +155,12 @@ def mosaic3d_telesto(
             res = da.rechunk(res, chunks=shard)
         else:
             res = da.rechunk(res, chunks=chunk)
-        zgroup = zarr.create_group(out, overwrite=True, zarr_format=zarr_config.zarr_version)
+        zarr_config.out = out
+        zgroup = from_config(zarr_config)
         zgroups.append(zgroup)
-        wconfig = default_write_config(op.join(out, '0'), shape=res.shape,
-                                       dtype=np.float32, chunk=chunk, shard=shard)
-        wconfig["create"] = True
-        wconfig["delete_existing"] = True
-        tswriter = ts.open(wconfig).result()
-        writers.append(tswriter)
+        writer = zgroup.create_array("0", shape=res.shape,
+                                     dtype=np.float32, zarr_config=zarr_config)
+        writers.append(writer)
         results.append(res)
     task = da.store(results, writers, compute=False)
 
@@ -170,11 +170,16 @@ def mosaic3d_telesto(
     scan_resolution = scan_resolution[:3][::-1]
     logger.info("finished")
     for zgroup in zgroups:
-        generate_pyramid(zgroup)
-        write_ome_metadata(zgroup, ["z", "y", "x"], scan_resolution, space_unit="millimeter")
-        nii_header = default_nifti_header(zgroup["0"], zgroup.attrs["multiscales"])
+        zgroup.generate_pyramid()
+        zgroup.write_ome_metadata(["z", "y", "x"], scan_resolution,
+                                  space_unit="millimeter")
+        write_ome_metadata(zgroup, ["z", "y", "x"], scan_resolution,
+                           space_unit="millimeter")
+        nii_header = default_nifti_header(zgroup["0"],
+                                          zgroup._get_zarr_python_group().attrs[
+                                              "multiscales"])
         nii_header.set_xyzt_units("mm")
-        # write_nifti_header(zgroup, nii_header)
+        zgroup.write_nifti_header(nii_header)
     logger.info("finished generating pyramid")
 
 
@@ -208,7 +213,7 @@ def temporary_compensation(dBI, R3D, O3D, depth, focus, scan_info):
     dBI = np.where(do_shift[..., None], dBI3D_shift, dBI)
     R3D = np.where(do_shift[..., None], R3D_shift, R3D)
     O3D = np.where(do_shift[..., None], O3D_shift, O3D)
-    return da.stack([dBI,R3D,O3D], 3)
+    return da.stack([dBI, R3D, O3D], 3)
 
 
 def build_slice(mosaic_idx, input_file_template, blend_ramp, clip_x, clip_y, full_width,
@@ -229,15 +234,17 @@ def build_slice(mosaic_idx, input_file_template, blend_ramp, clip_x, clip_y, ful
         complex3d = _load_tile(input_file_path)
         if complex3d.shape[0] > 4 * raw_tile_width:
             warnings.warn(
-                f"Complex3D shape {complex3d.shape} is larger than expected {4 * raw_tile_width}. "
-                "Trimming to 4*raw_tile_width."
-            )
+                    f"Complex3D shape {complex3d.shape} is larger than expected "
+                    f"{4 * raw_tile_width}. "
+                    "Trimming to 4*raw_tile_width."
+                    )
             complex3d = complex3d[:4 * raw_tile_width, :, :]
         elif complex3d.shape[0] < 4 * raw_tile_width:
             raise ValueError(
-                f"Complex3D shape {complex3d.shape} is smaller than expected {4 * raw_tile_width}. "
-                "Check the input file."
-            )
+                    f"Complex3D shape {complex3d.shape} is smaller than expected "
+                    f"{4 * raw_tile_width}. "
+                    "Check the input file."
+                    )
 
         dBI3D, R3D, O3D = process_complex3d(complex3d, raw_tile_width)
 
@@ -259,17 +266,17 @@ def build_slice(mosaic_idx, input_file_template, blend_ramp, clip_x, clip_y, ful
         coords.append((x0, y0))
 
     return stitch_tiles(
-        coords, tiles,
-        full_width=full_width, full_height=full_height,
-        depth=depth, blend_ramp=blend_ramp,
-        tile_size=(tile_width, tile_height),
-        chunk=chunk
-    )
+            coords, tiles,
+            full_width=full_width, full_height=full_height,
+            depth=depth, blend_ramp=blend_ramp,
+            tile_size=(tile_width, tile_height),
+            chunk=chunk
+            )
 
 
 def stitch_whole_canvas_padding(coords, tiles,
-                        full_width, full_height, depth,
-                        blend_ramp, tile_size, **_):
+                                full_width, full_height, depth,
+                                blend_ramp, tile_size, **_):
     """
     Build a full‐size zero‐canvas per tile and slice‐assign into it,
     then stack & sum (build_slice_whole_canvas_padding).
@@ -301,6 +308,7 @@ def stitch_whole_canvas_padding(coords, tiles,
     canvas = da.nan_to_num(canvas)
     return [canvas[..., i] for i in range(3)]
 
+
 def _combine_block(_, block_tiles, block_weights, *args, block_info=None, **kwargs):
     chunk_id = tuple(block_info[None]['chunk-location'][:2])
     paints = block_tiles[chunk_id]
@@ -312,7 +320,7 @@ def _combine_block(_, block_tiles, block_weights, *args, block_info=None, **kwar
 
     total_paint = da.sum(da.stack(paints, axis=0), axis=0)
     total_weight = da.sum(da.stack(weights, axis=0), axis=0)
-    return total_paint / total_weight[...,None,None]
+    return total_paint / total_weight[..., None, None]
 
 
 def stitch_tiles(coords, tiles,
@@ -368,18 +376,17 @@ def stitch_tiles(coords, tiles,
                 block_tiles[bid].append(block_canvas[sub_x, sub_y, ...])
                 block_weights[bid].append(block_weight[sub_x, sub_y])
 
-
-    canvas = da.map_blocks(_combine_block,canvas, block_tiles, block_weights,
-        dtype=canvas.dtype,
-        chunks=(pw, ph, depth, 3))
+    canvas = da.map_blocks(_combine_block, canvas, block_tiles, block_weights,
+                           dtype=canvas.dtype,
+                           chunks=(pw, ph, depth, 3))
 
     return [canvas[..., i] for i in range(3)]
 
 
 def process_complex3d(complex3d, raw_tile_width):
-    complex3d = complex3d.rechunk({0:raw_tile_width})
+    complex3d = complex3d.rechunk({0: raw_tile_width})
     comp = complex3d.reshape(
-        (4, raw_tile_width, complex3d.shape[1], complex3d.shape[2]))
+            (4, raw_tile_width, complex3d.shape[1], complex3d.shape[2]))
     j1r, j1i, j2r, j2i = comp[0], comp[1], comp[2], comp[3]
 
     j1 = j1r + 1j * j1i
@@ -387,11 +394,11 @@ def process_complex3d(complex3d, raw_tile_width):
     mag1 = da.abs(j1)
     mag2 = da.abs(j2)
 
-    dBI3D = da.flip(10 * da.log10(mag1**2 + mag2**2), axis=2)
+    dBI3D = da.flip(10 * da.log10(mag1 ** 2 + mag2 ** 2), axis=2)
     R3D = da.flip(
-        da.arctan(mag1 / mag2) / da.pi * 180,
-        axis=2
-    )
+            da.arctan(mag1 / mag2) / da.pi * 180,
+            axis=2
+            )
     offset = 100 / 180 * da.pi
     phi = da.angle(j1) - da.angle(j2) + offset * 2
     # wrap into [-π, π]
@@ -406,6 +413,7 @@ def do_downsample(volume: da.Array, factors: tuple) -> da.Array:
     fx, fy, fz = factors
     return da.coarsen(np.mean, volume, {0: fx, 1: fy, 2: fz}, trim_excess=True)
 
+
 def stack_slices(mosaic_info, slice_indices, slices):
     return da.concatenate(slices, axis=2)
     num_slices = len(slices)
@@ -417,7 +425,8 @@ def stack_slices(mosaic_info, slice_indices, slices):
     # --- Load one slice to get block size and build tissue profile ---
     Id0 = slices[0]
     if modality.lower() == 'mus':
-        # (If you want the 'mus' branch, you'd need skimage.filters.threshold_multiotsu, etc.)
+        # (If you want the 'mus' branch, you'd need
+        # skimage.filters.threshold_multiotsu, etc.)
         raise NotImplementedError("The 'mus' branch is not shown here.")
     else:
         # dBI: average a small tissue-only block
@@ -498,4 +507,3 @@ def stack_slices(mosaic_info, slice_indices, slices):
 
         Ma[:, :, zr1] += Id[:, :, zr2] * W
     return Ma
-
--- linc_convert/modalities/psoct/pyramid.py
+++ linc_convert/modalities/psoct/pyramid.py
@@ -1,33 +1,34 @@
 import logging
-from typing import Annotated, List
+from typing import List
 
 import cyclopts
 import zarr
-
-from linc_convert.modalities.psoct.cli import psoct
 from linc_convert.utils.zarr.generate_pyramid import generate_pyramid
 from linc_convert.utils.zarr.zarr_config import ZarrConfig
 from niizarr import default_nifti_header, write_nifti_header, write_ome_metadata
+
+from linc_convert.modalities.psoct.cli import psoct
 
 logger = logging.getLogger(__name__)
 pyramid = cyclopts.App(name="pyramid", help_format="markdown")
 psoct.command(pyramid)
 
+
 @pyramid.default
 def convert(
-    inp: List[str],
-    *,
-    zarr_config: ZarrConfig = None,
-    **kwargs
-) -> None:
+        inp: List[str],
+        *,
+        zarr_config: ZarrConfig = None,
+        **kwargs
+        ) -> None:
     for i in inp:
         print(i)
         zg = zarr.open_group(i, mode="r+")
         generate_pyramid(zg)
-        write_ome_metadata(zg, ["z", "y", "x"], [0.0025,0.01,0.01], space_unit="millimeter")
+        write_ome_metadata(zg, ["z", "y", "x"], [0.0025, 0.01, 0.01],
+                           space_unit="millimeter")
         nii_header = default_nifti_header(zg["0"], zg.attrs["multiscales"])
         nii_header.set_xyzt_units("mm")
         write_nifti_header(zg, nii_header)
 
     pass
-
