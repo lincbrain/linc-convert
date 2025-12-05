b10f06e85e43ff0c058c023fc251775b189833e7
Kaidong Chai <dev@kchai.me>
2025-12-05 11:17:36 -0500

    feat: add mosaic2d and mosaic3d to module

--- linc_convert/modalities/psoct/__init__.py
+++ linc_convert/modalities/psoct/__init__.py
@@ -4,8 +4,8 @@ try:
     import h5py as _h5py  # noqa: F401
     import scipy as _scipy  # noqa: F401
 
-    __all__ = ["cli", "multi_slice", "single_volume"]
+    __all__ = ["cli", "multi_slice", "single_volume", "mosaic2d", "mosaic3d"]
 
-    from . import cli, multi_slice, single_volume
+    from . import cli, multi_slice, single_volume, mosaic2d, mosaic3d
 except ImportError:
     pass
