fe95b2f330e9c022065cf23bdcd99270ab8ac5b5
Kaidong Chai <dev@kchai.me>
2025-12-16 14:34:24 -0500

    feat: Add stripe module for converting stripe spool files to Zarr format

--- linc_convert/modalities/lsm/__init__.py
+++ linc_convert/modalities/lsm/__init__.py
@@ -3,8 +3,8 @@
 try:
     import tifffile as _  # noqa: F401
 
-    __all__ = ["cli", "mosaic", "multi_slice", "spool"]
+    __all__ = ["cli", "mosaic", "multi_slice", "spool", "stripe"]
 
-    from . import cli, mosaic, multi_slice, spool
+    from . import cli, mosaic, multi_slice, spool, stripe
 except ImportError:
     pass
