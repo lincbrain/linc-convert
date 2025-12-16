1e0573d6f226724a74b65e1c30541715bed7cb97
Kaidong Chai <dev@kchai.me>
2025-12-16 17:02:18 -0500

    fix: line error when apply git

--- linc_convert/modalities/lsm/__init__.py
+++ linc_convert/modalities/lsm/__init__.py
@@ -3,8 +3,8 @@
 try:
     import tifffile as _  # noqa: F401
 
-    __all__ = ["cli", "mosaic", "multi_slice", "spool", "single_volume", "strip"]
+    __all__ = ["cli", "mosaic", "multi_slice", "spool", "single_volume", "stripe"]
 
-    from . import cli, mosaic, multi_slice, spool, single_volume, strip
+    from . import cli, mosaic, multi_slice, spool, single_volume, stripe
 except ImportError:
     pass
