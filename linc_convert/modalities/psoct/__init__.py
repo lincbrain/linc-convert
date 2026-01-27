5827f7fc48c8350c616ece40013cfd059bd2dd4f
Kaidong Chai <dev@kchai.me>
2026-01-27 15:10:45 -0500

    refactor: make the tile config generation to be a pipeline in psoct modalities

--- linc_convert/modalities/psoct/__init__.py
+++ linc_convert/modalities/psoct/__init__.py
@@ -4,8 +4,8 @@ try:
     import h5py as _h5py  # noqa: F401
     import scipy as _scipy  # noqa: F401
 
-    __all__ = ["cli", "multi_slice", "single_volume", "mosaic", "mosaic_complex"]
+    __all__ = ["cli", "multi_slice", "single_volume", "mosaic", "mosaic_complex", "generate_tile_config"]
 
-    from . import cli, mosaic, mosaic_complex, multi_slice, single_volume
+    from . import cli, mosaic, mosaic_complex, multi_slice, single_volume, generate_tile_config
 except ImportError:
     pass
